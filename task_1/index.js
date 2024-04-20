221756e01f1e6fc2fcf46c1ed680c4db27bdfbb9
Mat-Kon <mat_kon@mail.ru>
2024-04-20 21:21:53 +0300

    feat: finish task 1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,63 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const EXCEPTIONS = ['groupId', 'service', 'formatSize', 'ca'];
+const deepCopyEncoded = JSON.parse(JSON.stringify(encoded));
+
+const getListWithTranslationValue = (encoded, translations) => {
+  if (!encoded.length) {
+    throw new Error('Encoded list is empty');
+  }
+
+  const currentList = encoded.map((encodedItem) => {
+    for (const key in encodedItem) {
+      if (!isException(key) && hasEndId(key)) {
+        const encodedValue = encodedItem[key];
+        const translationsValue = translations[encodedValue];
+
+        if (translationsValue) {
+          encodedItem[key] = translationsValue
+        }
+      }
+    }
+    return encodedItem;
+  });
+
+  return currentList;
+};
+
+const isException = (value) => {
+  return EXCEPTIONS.find((exceptionValue) => value === exceptionValue);
+};
+
+const hasEndId = (value) => {
+  return value.endsWith('Id');
+};
 
 
+const getUniqueGroupIds = (encoded) => {
+  const uniqueGroupIds = new Set();
 
-// console.log(decoded)
+  encoded.forEach((item) => {
+    const keys = Object.keys(item);
+
+    keys.forEach((key) => {
+      if (hasEndId(key)) {
+        const value = item[key];
+        if (translations[value] !== undefined && translations[value] !== '') {
+          uniqueGroupIds.add(value);
+        }
+      }
+    });
+  });
+
+  return Array.from(uniqueGroupIds);
+};
+
+const uniqueGroupIds = getUniqueGroupIds(deepCopyEncoded);
+const decoded = getListWithTranslationValue(deepCopyEncoded, translations);
+
+console.log("Let's rock");
+console.log(encoded, translations);
+
+console.log(decoded);
+console.log('Уникальные groupId из массива encoded: ', uniqueGroupIds);
